65e5859cdab6308daedeb0829bc95711afcf9de5
Hamza HIKMAT <hamzahik@gmail.com>
2016-10-24 10:07:37 +0200

    enabling Cors

--- home/homeController.js
+++ home/homeController.js
@@ -1,6 +1,14 @@
 'use strict';
 
 angular.module('wakanda-server-doc')
-  .controller('homeController', function($scope) {
-
+  .controller('homeController', function ($scope) {
+    var sections = [
+      {
+        name: 'Data Integration',
+        description: 'Wakanda has an efficient relational data model, that allows you to work seamlessly with any datasource.',
+        imageUrl: './images/datamodel.png',
+        link: '#/doc/data'
+      }
+    ];
+    $scope.sections = sections;
   });
